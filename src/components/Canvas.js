099f02556de144180d956e3b73c272fb71373528
Dario Lopes <dariolopes4v@gmail.com>
2023-10-09 02:51:16 +0200

    fix: upload box wrong look without files

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -220,9 +220,9 @@ export default function Canvas() {
                     <div className="flex items-center justify-center w-full relative">
                         <label
                             htmlFor="dropzone-file"
-                            className={`flex flex-col items-center justify-center w-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 ${
+                            className={`flex flex-col items-center justify-center w-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 transition-colors dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 ${
                                 dragActive ? 'dark:bg-gray-600 bg-gray-200' : ''
-                            } ${files ? 'h-32 mt-5' : 'h-64'}`}
+                            } ${files.length > 0 ? 'h-32 mt-5' : 'h-64'}`}
                             onMouseOut={() => setDragActive(false)}
                             onDragEnter={handleUploadDrag}
                             onDragLeave={handleUploadDrag}
@@ -233,7 +233,7 @@ export default function Canvas() {
                                 <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
                                 </svg>
-                                {files ? (
+                                {files.length > 0 ? (
                                     <>
                                         <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
                                             <span className="font-semibold">Click to upload</span> or drag and drop <span className="font-semibold">more</span>
